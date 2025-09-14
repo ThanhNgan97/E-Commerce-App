db2f7c075d1a926210cd5673539e2af06f6eac52
ThanhNgan97 <nguyenthanhngan09072002@gmail.com>
2025-09-14 22:15:44 +0700

    CartModal of components

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -12,13 +12,18 @@ const CartModal = () => {
             !cartItems ? (
                 <div className="">Cart is Empty</div>
             ): (
+                <>
+                <h2 className="text-xl">Shpping Cart</h2>
+                 {/* LIST */}
+                <div className="flex flex-col gap-8">
+                    {/* ITEMS */}
                 <div className="flex gap-4">
                     <Image src="https://images.pexels.com/photos/33419665/pexels-photo-33419665.jpeg" 
                         alt="" 
                         width={72} 
                         height={96} 
                         className="object-cover rounded-md"
-                    />
+                        />
                     <div className="flex flex-col justify-between w-full">
                         {/* TOP */}
                         <div className="">
@@ -36,7 +41,51 @@ const CartModal = () => {
                             <span className="text-blue-500">Remove</span>
                         </div>
                     </div>
+                        </div>
+
+                            {/* ITEMS */}
+                          <div className="flex gap-4">
+                    <Image src="https://images.pexels.com/photos/33419665/pexels-photo-33419665.jpeg" 
+                        alt="" 
+                        width={72} 
+                        height={96} 
+                        className="object-cover rounded-md"
+                        />
+                    <div className="flex flex-col justify-between w-full">
+                        {/* TOP */}
+                        <div className="">
+                        {/* TITLE */}
+                        <div className="flex items-center justify-between gap-8">
+                            <h3 className="font-semibold">Product Name</h3>
+                            <div className="p-1 bg-gray-50 rounded-sm">$49</div>
+                        </div>
+                        {/* DESC */}
+                        <div className="text-sm text-gray-500">available</div>
+                        </div>
+                        {/* BOTTOM */}
+                        <div className="flex justify-between text-sm">
+                            <span className="text-gray-500">Qty.2</span>
+                            <span className="text-blue-500">Remove</span>
+                        </div>
+                    </div>
+                        </div>
                 </div>
+
+                {/* BOTTOM */}
+                <div className="">
+                    <div className="flex items-center justify-between font-semibold">
+                        <span className="">Subtotal</span>
+                        <span className="">$49</span>
+                    </div>
+                    <p className="text-gray-500 text-sm mt-2 mb-4">
+                        Shipping and taxes calculated at checkout.
+                    </p>
+                    <div className="flex justify-between text-sm">
+                        <button className="rounded-md py-3 px-4 ring-1 ring-gray-300">View Cart</button>
+                        <button className="rounded-md py-3 px-4 bg-black text-white">Check out </button>
+                    </div>
+                </div>
+            </>
             )
         }
     </div>
